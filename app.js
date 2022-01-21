5f5f17ec673cf7c42a2782ef093830459d01b3ea
rachelharu <rachel.haru9@gmail.com>
2022-01-21 13:10:46 -0800

    set up mongoose encryption

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 
 const app = express();
 
@@ -11,6 +13,18 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
+
+const userSchema = new mongoose.Schema ({
+  email: String,
+  password: String
+});
+
+const secret = "Thisisourlittlesecret.";
+userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"]});
+
+
+const User = new mongoose.model("User", userSchema);
 
 app.get("/", (req, res) => {
   res.render("home");
@@ -24,6 +38,36 @@ app.get("/register", (req, res) => {
   res.render("register");
 });
 
+app.post("/register", (req, res) => {
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password
+  });
+
+  newUser.save((err)=>{
+    if (err){
+      console.log(err);
+    } else {
+      res.render("secrets");
+    }
+  });
+});
+
+app.post("/login", (req, res)=>{
+  const username = req.body.username;
+  const password = req.body.password;
+  User.findOne({email: username}, (err, foundUser)=>{
+    if (err){
+      console.log(err);
+    } else {
+      if (foundUser){
+        if (foundUser.password === password) {
+          res.render("secrets");
+        }
+      }
+    }
+  });
+});
 
 
 app.listen(3000, ()=>{
